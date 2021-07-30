6c504d4f02af268362b6bc5dd40df88a88f2b6e8
lukaskreibig <lukatze@posteo.de>
2021-07-30 16:19:09 +0200

    made it work for one word (use double space to trigger)

--- src/components/AutocorrectTextarea.js
+++ src/components/AutocorrectTextarea.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
 
-class AutocorrectTextarea extends React.Component {
+function AutocorrectTextarea({ corrections }) {
+  const [autoCorrection, setAutoCorrection] = useState("");
+  const [lastWord, setLastWord] = useState("");
 
-  render() {
-    return (
+  const correctionHandler = () => {
+    if (autoCorrection.length > 0) {
+      setLastWord(autoCorrection);
+      for (let key in corrections) {
+        console.log(key);
+        console.log(corrections[key]);
+        if (key === lastWord) {
+          setAutoCorrection(corrections[key]);
+          console.log(lastWord);
+        }
+      }
+    }
+  };
+
+  return (
+    <div>
       <div className="text-center">
-        <textarea data-testid="textarea" rows={10} cols={80} className="card" />
+        <textarea
+          data-testid="textarea"
+          value={autoCorrection}
+          onChange={(e) => {
+            setAutoCorrection(e.target.value);
+            correctionHandler();
+          }}
+          rows={10}
+          cols={80}
+          className="card"
+        />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default AutocorrectTextarea;
